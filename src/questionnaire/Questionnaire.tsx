755827e8904bc4a465f6baf3719e786b8ad4da48
Tsuyoshi HARA <chloe463+git@gmail.com>
2020-03-02 01:29:21 +0900

    feat: Make it possible to update questionnaire title

--- src/questionnaire/Questionnaire.tsx
+++ src/questionnaire/Questionnaire.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import gql from "graphql-tag";
 
@@ -27,14 +27,35 @@ export const QUESTIONNAIRE_FRAGMENT = gql`
 
 interface Props {
   questionnaire: QuestionnaireFragment;
+  submit: (id: number, title: string) => void;
 }
 
 export const QuestionnaireComponent: React.FC<Props> = props => {
-  const { questionnaire } = props;
+  const { questionnaire, submit } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(questionnaire.title);
   return (
     <Wrapper>
       <Item>
-        {questionnaire.id}:{questionnaire.title}
+        {questionnaire.id}
+        {isEditing ? (
+          <TextField value={title} onChange={e => setTitle(e.target.value)} />
+        ) : (
+          `${questionnaire.title}`
+        )}
+        <EditToggleButton onClick={() => setIsEditing(v => !v)}>
+          {isEditing ? `Cancel` : `Edit`}
+        </EditToggleButton>
+        {isEditing && (
+          <SubmitButton
+            onClick={() => {
+              setIsEditing(false);
+              submit(questionnaire.id, title);
+            }}
+          >
+            Done
+          </SubmitButton>
+        )}
       </Item>
     </Wrapper>
   );
@@ -43,3 +64,9 @@ export const QuestionnaireComponent: React.FC<Props> = props => {
 const Wrapper = styled.div``;
 
 const Item = styled.li``;
+
+const TextField = styled.input.attrs({ type: "text" })``;
+
+const EditToggleButton = styled.button``;
+
+const SubmitButton = styled.button``;
--- src/questionnaire/QuestionnaireContainer.tsx
+++ src/questionnaire/QuestionnaireContainer.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
 import { Questionnaires } from "./__generated__/Questionnaires";
+import { QuestionnaireFragment } from "./__generated__/QuestionnaireFragment";
+import {
+  UpdateQuestionnaire,
+  UpdateQuestionnaireVariables
+} from "./__generated__/UpdateQuestionnaire";
 import {
   QUESTIONNAIRE_FRAGMENT,
   QuestionnaireComponent
@@ -17,8 +22,45 @@ const GET_QUESTIONNAIRES = gql`
   ${QUESTIONNAIRE_FRAGMENT}
 `;
 
+const UPDATE_QUESTIONNAIRE = gql`
+  mutation UpdateQuestionnaire($questionnaire: UpdateQuestionnaireInput!) {
+    updateQuestionnaire(questionnaire: $questionnaire) {
+      id
+      success
+    }
+  }
+`;
+
 export const QuestionnaireContainer = () => {
   const { data, loading } = useQuery<Questionnaires>(GET_QUESTIONNAIRES);
+  const [updateQuestionnaireFn] = useMutation<
+    UpdateQuestionnaire,
+    UpdateQuestionnaireVariables
+  >(UPDATE_QUESTIONNAIRE);
+  const submit = async (id: number, title: string) => {
+    await updateQuestionnaireFn({
+      variables: {
+        questionnaire: {
+          id,
+          title
+        }
+      },
+      update: proxy => {
+        const current = proxy.readFragment<QuestionnaireFragment>({
+          id: `Questinnaire:${id}`,
+          fragment: QUESTIONNAIRE_FRAGMENT
+        });
+        proxy.writeFragment({
+          id: `Questionnaire:${id}`,
+          fragment: QUESTIONNAIRE_FRAGMENT,
+          data: {
+            ...current,
+            title
+          }
+        });
+      }
+    });
+  };
   return (
     <>
       {loading || !data ? (
@@ -26,7 +68,13 @@ export const QuestionnaireContainer = () => {
       ) : (
         <>
           {data.questionnaires.map(q => {
-            return <QuestionnaireComponent key={q.id} questionnaire={q} />;
+            return (
+              <QuestionnaireComponent
+                key={q.id}
+                questionnaire={q}
+                submit={submit}
+              />
+            );
           })}
         </>
       )}
